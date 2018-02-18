dfcfdbc54b52822aac68540ae903a7b60c749a5d
Sulejman Sarajlija <sulejmansarajlija@gmail.com>
2018-02-18 18:54:16 +0100

    New tokeeens

--- src/config/tokens/tokenconfigmain.ts
+++ src/config/tokens/tokenconfigmain.ts
@@ -64,4 +64,72 @@ export const TokenConfigMain = {
     code: "",
     contractAddress: "0x3597bfd533a99c9aa083587b074434e61eb0a258"
   },
+  Populous: {
+    code: "",
+    contractAddress: "0x374f78f2f4001ebc22b8a7d8045f165b43712788"
+  },
+  Maker: {
+    code: "",
+    contractAddress: "0xe17cb0056d6ac5deff3be5b94cd971ac07f48031"
+  },
+  DigixDAO: {
+    code: "",
+    contractAddress: "0xc40a46ca4bc8e6057ed571e39cf400f3f935e4d5"
+  },
+  QASH: {
+    code: "",
+    contractAddress: "0x89a0d2de3f88c0b375eccf5f99a667d3c3aa105e"
+  },
+  Ethos: {
+    code: "",
+    contractAddress: "0x30bfab2a827b71e2e6973d4b226179a56c1d5312"
+  },
+  FunFair: {
+    code: "",
+    contractAddress: "0x2e43b3eaaf56302d0f21e41b3065694e1c8a8f35"
+  },
+  Ox: {
+    code: "",
+    contractAddress: "0x334ca5ce83a12207a3677eec2e723f4b0dee7f40"
+  },
+  RequestNetwork: {
+    code: "",
+    contractAddress: "0xca401b69438a86d51a01b57788fdf007bd618ac7"
+  },
+  Bancor : {
+    code: "",
+    contractAddress: "0x962a2eaebda0602817a4994f51f1cebb7718ae62"
+  },
+  Iconomi: {
+    code: "",
+    contractAddress: "0x623a890c0f1da0487084a8dc09c50133a2f7314f"
+  },
+  TenXPay: {
+    code: "",
+    contractAddress: "0x0992983961841516d10edad2d35764ca4eda3022"
+  },
+  Storj: {
+    code: "",
+    contractAddress: "0xff5ada2bea34247da87f0fd26c24ead2b64edebf"
+  },
+  EnjinCoin: {
+    code: "",
+    contractAddress: "0x9c37719657bbb2ff8683da9aae117aeb9ed02389"
+  },
+  Monaco: {
+    code: "",
+    contractAddress: "0xbe02615ea60a7e2832a1548dcf86573e11b11fa1"
+  },
+  Edgeless: {
+    code: "",
+    contractAddress: "0x048076b8c4d27149ad76a5b56e41b8dc1d1f2305"
+  },
+  VeChain: {
+    code: "",
+    contractAddress: "0x832b52302b89fa8e703cc12db1b6049984d6fef7"
+  },
+  ICON: {
+    code: "",
+    contractAddress: "0xc7683b60eca623449d61d37433501bcbdaaf5bba3284bb4c063064ed8a898f58"
+  },
 };
