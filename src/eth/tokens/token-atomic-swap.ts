e559cf6d80f1ab0affeb4ac4635d4c7b68fcf8ef
Sulejman Sarajlija <sulejmansarajlija@gmail.com>
2018-02-18 22:23:16 +0100

    amount

--- src/eth/tokens/token-atomic-swap.ts
+++ src/eth/tokens/token-atomic-swap.ts
@@ -30,6 +30,7 @@ export class TokenAtomicSwap extends ERC20 {
         console.log("ETH TOKEN PARTICIPATE PARAMS: ", partParams);
 
         const amount = this.ethEngine.toWei(parseFloat(partParams.amount), "ether");
+        // tslint:disable-next-line
         console.log("New amount: ",amount);
         const approveResult = await this.approve(TokenConfig.AtomicSwap.contractAddress, amount);
 // const transferResult = this.transfer(TokenConfig.AtomicSwap.contractAddress,
@@ -63,7 +64,8 @@ export class TokenAtomicSwap extends ERC20 {
         // tslint:disable-next-line
         console.log("ETH TOKEN INITIATE PARAMS: ", initParams);
 
-        const amount = this.ethEngine.toWei(parseFloat(initParams.amount), "ether"); 
+        const amount = this.ethEngine.toWei(parseFloat(initParams.amount), "ether");
+        // tslint:disable-next-line
         console.log("New amount: ", amount);
 
         const approveResult = await this.approve(TokenConfig.AtomicSwap.contractAddress, amount);
